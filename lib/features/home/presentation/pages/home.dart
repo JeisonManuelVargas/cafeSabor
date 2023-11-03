1a87c2c647789ad5cda3e2a53c79070d5a28c47a
manuel vargas <jeisonvargas@flutterlabapps.com>
2023-11-02 22:56:00 -0500

    implemented drawer

--- lib/features/home/presentation/pages/home.dart
+++ lib/features/home/presentation/pages/home.dart
@@ -5,6 +5,7 @@ import 'package:cafe_sabor/core/base/base_page.dart';
 import 'package:cafe_sabor_ui_kit/cafe_sabor_ui_kit.dart';
 import 'package:cafe_sabor/core/extension/context_extension.dart';
 import 'package:cafe_sabor/features/home/presentation/cubit/home_cubit.dart';
+import 'package:cafe_sabor/features/home/presentation/widget/custom_drawer.dart';
 import 'package:cafe_sabor/features/home/presentation/widget/carousel_products_widget.dart';
 import 'package:cafe_sabor/features/home/presentation/widget/animated_description_widget.dart';
 
@@ -17,6 +18,7 @@ class Home extends BasePage<HomeState, HomeCubit> {
   @override
   Widget buildPage(BuildContext context, state, bloc) {
     return Scaffold(
+      endDrawer: const CustomDrawer(),
       body: SizedBox(
         width: context.sizeWidth(),
         height: context.sizeHeight(),
@@ -40,9 +42,26 @@ class Home extends BasePage<HomeState, HomeCubit> {
                       SizedBox(height: context.sizeHeight() * 0.09),
                       Padding(
                         padding: const EdgeInsets.symmetric(horizontal: 33),
-                        child: Text(
-                          "JEISON IT'S\nCOFFEE TIME".toUpperCase(),
-                          style: CafeKit.util.cafeTextStyle.titleL,
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              "JEISON IT'S\nCOFFEE TIME".toUpperCase(),
+                              style: CafeKit.util.cafeTextStyle.titleL,
+                            ),
+                            Builder(builder: (context) {
+                              return InkWell(
+                                onTap: () =>
+                                    Scaffold.of(context).openEndDrawer(),
+                                child: Icon(
+                                  Icons.menu,
+                                  color: CafeKit.util.color.backgroundButton,
+                                  size: 30,
+                                ),
+                              );
+                            })
+                          ],
                         ),
                       ),
                     ],
@@ -72,8 +91,10 @@ class Home extends BasePage<HomeState, HomeCubit> {
               SizedBox(height: context.sizeHeight() * 0.02),
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 30),
-                child: CafeKit.widget.button.formButton(
-                  ButtonModel(label: "Suscribe", onTap: () {}),
+                child: Builder(
+                  builder: (context) => CafeKit.widget.button.formButton(
+                    ButtonModel(label: "Suscribe", onTap: () {}),
+                  ),
                 ),
               )
             ],
---
+++ lib/features/home/presentation/widget/custom_drawer.dart
@@ -0,0 +1,127 @@
+// ignore_for_file: depend_on_referenced_packages
+
+import 'package:cafe_sabor/core/extension/context_extension.dart';
+import 'package:flutter/material.dart';
+import 'package:cafe_sabor_ui_kit/cafe_sabor_ui_kit.dart';
+
+class CustomDrawer extends StatelessWidget {
+  const CustomDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      child: Material(
+        color: CafeKit.util.color.backgroundButton,
+        borderRadius: const BorderRadius.only(
+            bottomLeft: Radius.circular(30), topLeft: Radius.circular(30)),
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            vertical: context.sizeHeight() * 0.1,
+            horizontal: context.sizeWidth() * 0.09,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                alignment: Alignment.centerRight,
+                child: IconButton(
+                  onPressed: () => Scaffold.of(context).closeEndDrawer(),
+                  icon: Icon(
+                    Icons.close,
+                    color: CafeKit.util.color.whiteCard,
+                  ),
+                ),
+              ),
+              SizedBox(height: context.sizeHeight() * 0.07),
+              _generateOptions(
+                onTap: () {},
+                title: "PROFILE",
+                context: context,
+                icon: Icons.person_2_outlined,
+              ),
+              SizedBox(height: context.sizeHeight() * 0.02),
+              _generateOptions(
+                onTap: () {},
+                title: "PRODUCTS",
+                context: context,
+                icon: Icons.shopping_bag_outlined,
+              ),
+              SizedBox(height: context.sizeHeight() * 0.02),
+              _generateOptions(
+                onTap: () {},
+                title: "RECIPES",
+                context: context,
+                icon: Icons.coffee_outlined,
+              ),
+              SizedBox(height: context.sizeHeight() * 0.02),
+              _generateOptions(
+                  onTap: () {},
+                  title: "LOGOUT",
+                  context: context,
+                  icon: Icons.close,
+                  customIcon: Container(
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(
+                        color: CafeKit.util.color.red,
+                        width: 2,
+                      ),
+                    ),
+                    padding: const EdgeInsets.all(2),
+                    child: Icon(
+                      Icons.close,
+                      color: CafeKit.util.color.red,
+                      size: 13,
+                    ),
+                  )),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _generateOptions({
+    Widget? customIcon,
+    required IconData icon,
+    required String title,
+    required Function onTap,
+    required BuildContext context,
+  }) =>
+      InkWell(
+        borderRadius: BorderRadius.circular(10),
+        onTap: () {},
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: context.sizeHeight() * 0.02),
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Flexible(
+                fit: FlexFit.tight,
+                flex: 1,
+                child: Container(
+                  alignment: Alignment.centerRight,
+                  child: customIcon ?? Icon(icon),
+                ),
+              ),
+              Flexible(
+                flex: 3,
+                fit: FlexFit.tight,
+                child: Container(
+                  alignment: Alignment.centerRight,
+                  child: Text(
+                    title,
+                    style: CafeKit.util.cafeTextStyle.description.copyWith(
+                        fontWeight: FontWeight.w300,
+                        color: CafeKit.util.color.whiteCard),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      );
+}
